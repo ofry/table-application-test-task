8a0909609d0742267ace9594cfbb80dce673067f
ofry <tim4job@bmail.ru>
2020-10-08 03:08:32 +0300

    Багфикс.
    Signed-off-by: ofry <tim4job@bmail.ru>

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -31,7 +31,7 @@ return [
             'orm_default' => [
                 'drivers' => [
                     // register `my_annotation_driver` for any entity under namespace `My\Namespace`
-                    'Application/Model/Entity' => 'my_annotation_driver',
+                    'Application\Model\Entity' => 'my_annotation_driver',
                 ],
             ],
         ],
